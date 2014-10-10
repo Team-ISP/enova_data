6700a251c375a48aacc4fa87c8cf5418075d25d2
yulunwu1 <yulunwu2013@u.northwestern.edu>
2014-10-10 18:59:14 -0500

    update

--- flash drive input/data/yulun-parser.py
+++ flash drive input/data/yulun-parser.py
@@ -4,5 +4,12 @@ for line in input_file:
 	line=line.strip()
 	split_string=line.split(";")
 	array.append(split_string)
-print array[0]
-print array[1]
+x=array[0]
+y=array[1]
+
+cap=len(array[0])
+counter=0
+while (counter<cap):
+	print x[counter]
+	print y[counter]
+	counter+=1
